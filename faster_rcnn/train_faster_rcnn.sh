ee245d7483c462dd4d45f92ec94c58393083e319
Aakash Rao <mrao.aakash@gmail.com>
2023-11-29 13:24:41 +0530

    added config files

--- faster_rcnn/train_faster_rcnn.sh
+++ faster_rcnn/train_faster_rcnn.sh
@@ -4,6 +4,5 @@ python3 train_faster_rcnn.py \
     --max_iters 20000 \
     --name faster_rcnn_r50_fpn_1x_coco \
     --fold 1 \
-    --project capstone-project \
     --save_path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs \
 
