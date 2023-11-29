18fc7a1fb06bbd0467205d26346e483dba12f847
Aakash Rao <mrao.aakash@gmail.com>
2023-11-29 10:48:47 +0530

    updated gitignore

--- yolo/utils/data_formatter.py
+++ yolo/utils/data_formatter.py
@@ -3,6 +3,7 @@ import numpy
 import pandas as pd
 import cv2
 import argparse
+import shutil
 
 
 image_dir = ''
@@ -15,6 +16,12 @@ def image_info(image_dir, mask_dir, save_dir, phase):
     elif len(os.listdir(image_dir)) == 0:
         raise ValueError("image_dir is empty")
     else:
+        im_save_dir = os.path.join(save_dir, 'images')
+        mask_save_dir = os.path.join(save_dir, 'masks')
+        if not os.path.exists(im_save_dir):
+            os.makedirs(im_save_dir)
+        if not os.path.exists(mask_save_dir):
+            os.makedirs(mask_save_dir)
         for image_name in os.listdir(image_dir):
             image_path = os.path.join(image_dir, image_name)
             mask_path = os.path.join(mask_dir, image_name.split('.png')[0] + '.csv')
@@ -39,6 +46,12 @@ def image_info(image_dir, mask_dir, save_dir, phase):
                 # print(x_min, y_min, x_max, y_max, class_name, class_id)
                 yolo_format = f"{class_id} {x_min} {y_min} {x_max} {y_max}"
                 print(yolo_format)
+
+                with open(os.path.join(im_save_dir, image_name.split('.png')[0] + '.txt'), 'a') as f:
+                    f.write(yolo_format + '\n')
+                shutil.copy(image_path, im_save_dir)
+                
+
 
             if phase == 'testing':
                 result = "testing complete"
