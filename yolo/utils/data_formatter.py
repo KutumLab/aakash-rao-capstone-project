f45a4726fe114b71a6bed1db0cafb8a20274f89c
Aakash Rao <mrao.aakash@gmail.com>
2023-11-29 11:01:33 +0530

    updated gitignore

--- yolo/utils/data_formatter.py
+++ yolo/utils/data_formatter.py
@@ -111,5 +111,5 @@ if __name__ == '__main__':
     args = argparser.parse_args()
 
     # result = image_info(args.image_dir, args.mask_dir, args.save_dir, args.phase)
-    make_folds(os.path.join (args.save_dir, 'master', 'images'), os.path.join (args.save_dir, 'master', 'masks'), args.save_dir, args.folds)
+    make_folds(os.path.join (args.save_dir, 'master', 'images'), os.path.join (args.save_dir, 'master', 'masks'), args.save_dir, int(args.folds), int(args.seed))
     # print(result)
