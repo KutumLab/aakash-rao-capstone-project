e997c9f3589ba098532754a320132158dd3d778c
Aakash Rao <mrao.aakash@gmail.com>
2023-11-29 13:41:17 +0530

    added config files

--- faster_rcnn/train_faster_rcnn.sh
+++ faster_rcnn/train_faster_rcnn.sh
@@ -1,7 +1,7 @@
 python3 train_faster_rcnn.py \
     --data_path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/detectron \
     --config_info COCO-Detection/faster_rcnn_R_50_DC5_1x.yaml \
-    --max_iters 20000 \
+    --max_iters 1 \
     --name faster_rcnn_R_50_DC5_1x \
     --fold 1 \
     --save_path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs \
