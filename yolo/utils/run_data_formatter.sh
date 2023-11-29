65ddc68c287e849cb7621dce76380ef2047682ed
Aakash Rao <mrao.aakash@gmail.com>
2023-11-29 11:01:00 +0530

    updated gitignore

--- yolo/utils/run_data_formatter.sh
+++ yolo/utils/run_data_formatter.sh
@@ -4,3 +4,5 @@ python3 data_formatter.py \
     -m /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/csv \
     -s /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/yolov5 \
     -p run \
+    -f 3 \
+    --seed 42 \
