dcc55740c1bf3d1811908a861c6e1d5719246220
Aakash Rao <mrao.aakash@gmail.com>
2023-11-29 12:59:55 +0530

    added config files

--- faster_rcnn/utils/data_formatter.py
+++ faster_rcnn/utils/data_formatter.py
@@ -177,5 +177,5 @@ if __name__ == '__main__':
     args = argparser.parse_args()
 
     # image_info(args.image_dir, args.mask_dir, args.save_dir, args.phase)
-    make_folds(os.path.join(args.save_dir, 'master', 'master.npy'), args.save_dir, int(args.folds), int(args.seed))
-    # plot_num_classes(os.path.join(args.save_dir,'master', 'num_classes_per_image.npy'))
+    # make_folds(os.path.join(args.save_dir, 'master', 'master.npy'), args.save_dir, int(args.folds), int(args.seed))
+    plot_num_classes(os.path.join(args.save_dir,'master', 'num_classes_per_image.npy'))
