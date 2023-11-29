17679c7490378d9abdc724c1e7c5e1759f3d28c0
Aakash Rao <mrao.aakash@gmail.com>
2023-11-29 11:44:53 +0530

    added config files

--- yolo/utils/data_formatter.py
+++ yolo/utils/data_formatter.py
@@ -62,11 +62,10 @@ def make_folds(image_dir, mask_dir, save_dir, folds,seed=42):
                 pass
             train_images = images[:i*len_of_each_fold] + images[(i+1)*len_of_each_fold:]
             test_images = images[i*len_of_each_fold:(i+1)*len_of_each_fold]
-            print(len(train_images), len(test_images))
 
             for image in tqdm (train_images, desc="Creating Train...", ascii=False, ncols=75):
-                im_save_path = os.path.join(im_save_dir,'train')
-                mask_save_path = os.path.join(mask_save_dir,'train')
+                im_save_path = os.path.join(im_save_dir,'train', 'images')
+                mask_save_path = os.path.join(mask_save_dir,'train', 'labels')
                 if not os.path.exists(im_save_path):
                     os.makedirs(im_save_path)
                 if not os.path.exists(mask_save_path):
@@ -77,8 +76,8 @@ def make_folds(image_dir, mask_dir, save_dir, folds,seed=42):
                 shutil.copy(image_path, im_save_path)
                 shutil.copy(mask_path, mask_save_path)
             for image in tqdm (test_images, desc="Creating Test...", ascii=False, ncols=75):
-                im_save_path = os.path.join(im_save_dir,'test')
-                mask_save_path = os.path.join(mask_save_dir,'test')
+                im_save_path = os.path.join(im_save_dir,'test', 'images')
+                mask_save_path = os.path.join(mask_save_dir,'test', 'labels')
                 if not os.path.exists(im_save_path):
                     os.makedirs(im_save_path)
                 if not os.path.exists(mask_save_path):
@@ -99,7 +98,7 @@ def image_info(image_dir, mask_dir, save_dir, phase):
     else:
         save_dir = os.path.join(save_dir, "master")
         im_save_dir = os.path.join(save_dir, 'images')
-        mask_save_dir = os.path.join(save_dir, 'masks')
+        mask_save_dir = os.path.join(save_dir, 'labels')
         if not os.path.exists(im_save_dir):
             os.makedirs(im_save_dir)
         if not os.path.exists(mask_save_dir):
@@ -159,5 +158,5 @@ if __name__ == '__main__':
     args = argparser.parse_args()
 
     # image_info(args.image_dir, args.mask_dir, args.save_dir, args.phase)
-    make_folds(os.path.join (args.save_dir, 'master', 'images'), os.path.join (args.save_dir, 'master', 'masks'), args.save_dir, int(args.folds), int(args.seed))
+    make_folds(os.path.join (args.save_dir, 'master', 'images'), os.path.join (args.save_dir, 'master', 'labels'), args.save_dir, int(args.folds), int(args.seed))
     plot_num_classes(os.path.join(args.save_dir,'master', 'num_classes_per_image.npy'))
