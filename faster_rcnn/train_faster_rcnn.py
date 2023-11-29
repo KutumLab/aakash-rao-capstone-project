cc1772fc4f8f003dbca6ef088b8d31770929c1e9
Aakash Rao <mrao.aakash@gmail.com>
2023-11-29 13:26:44 +0530

    added config files

--- faster_rcnn/train_faster_rcnn.py
+++ faster_rcnn/train_faster_rcnn.py
@@ -48,6 +48,7 @@ def set_config(config_info, fold, max_iters, data_path, name,save_path):
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4 
     cfg.OUTPUT_DIR = os.path.join(save_path, f'detectron/{name}')
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 
 
     # os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
