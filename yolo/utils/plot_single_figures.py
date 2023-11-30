861918f86988afdf1a5d18f121767ac80b2ec8b4
Aakash Rao <mrao.aakash@gmail.com>
2023-11-30 10:38:56 +0530

    test format

--- yolo/utils/plot_single_figures.py
+++ yolo/utils/plot_single_figures.py
@@ -4,6 +4,21 @@ import pandas
 import matplotlib.pyplot as plt
 import argparse
 
+keys = ['               epoch', '      train/box_loss', '      train/obj_loss',
+       '      train/cls_loss', '   metrics/precision', '      metrics/recall',
+       '     metrics/mAP_0.5', 'metrics/mAP_0.5:0.95', '        val/box_loss',
+       '        val/obj_loss', '        val/cls_loss', '               x/lr0',
+       '               x/lr1', '               x/lr2']
+
+translation_array = ['epoch', 'train_box_loss', 'train_obj_loss',
+         'train_cls_loss', 'metrics_precision', 'metrics_recall',
+         'metrics_mAP_0.5', 'metrics_mAP_0.5:0.95', 'val_box_loss',
+         'val_obj_loss', 'val_cls_loss', 'lr0',
+         'lr1', 'lr2']
+translation_dict = dict(zip(keys, translation_array))
+relevant_keys = ['epoch', 'train_box_loss', 'train_obj_loss', 'train_cls_loss', 'metrics_precision', 'metrics_recall', 'metrics_mAP_0.5', 'metrics_mAP_0.5:0.95']
+plot_titles = ['Box loss', 'Object loss', 'Class loss', 'Precision', 'Recall', 'mAP at IoU=0.5', 'mAP at IoU=0.5:0.95']
+axis_labels = ['Epoch', 'Loss', 'Loss', 'Loss', 'Precision', 'Recall', 'mAP', 'mAP']
 
 
 def plot(src_path, phase):
@@ -20,6 +35,9 @@ def plot(src_path, phase):
             plot_save_path = os.path.join(outpath, "plot")
             if not os.path.exists(plot_save_path):
                 os.makedirs(plot_save_path)
+            results = results.rename(columns=translation_dict)
+            results = results[relevant_keys]
+
             if phase == "testing":
                 print(results.columns)
                 return
