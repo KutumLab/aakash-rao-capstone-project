a1797af31af98c5dbbc88c3dedbd2b2e55ce2891
Aakash Rao <mrao.aakash@gmail.com>
2023-12-01 16:47:42 +0530

    added path

--- cascade_rcnn/utils/data_formatter.py
+++ cascade_rcnn/utils/data_formatter.py
@@ -39,26 +39,35 @@ def generate_coco_dataset(data_path, fold):
         data = list(data)
         print(f'Number of test images: {len(data)}')
         return data
+    
+    try:
+        DatasetCatalog.register(f'fold_{fold}_train', data_train)
+        MetadataCatalog.get(f'fold_{fold}_train').thing_classes = ['nonTIL_stromal','sTIL','tumor_any','other']
+        MetadataCatalog.get(f'fold_{fold}_train').thing_colors = [(161,9,9),(239,222,0),(22,181,0),(0,32,193),(115,0,167)]
+    except:
+        pass
+    
+    try:
+        DatasetCatalog.register(f'fold_{fold}_val', data_val)
+        MetadataCatalog.get(f'fold_{fold}_val').thing_classes = ['nonTIL_stromal','sTIL','tumor_any','other']
+        MetadataCatalog.get(f'fold_{fold}_val').thing_colors = [(161,9,9),(239,222,0),(22,181,0),(0,32,193),(115,0,167)]
+    except:
+        pass
 
-    DatasetCatalog.register(f'fold_{fold}_train', data_train)
-    MetadataCatalog.get(f'fold_{fold}_train').thing_classes = ['nonTIL_stromal','sTIL','tumor_any','other']
-    MetadataCatalog.get(f'fold_{fold}_train').thing_colors = [(161,9,9),(239,222,0),(22,181,0),(0,32,193),(115,0,167)]
-
-
-    DatasetCatalog.register(f'fold_{fold}_val', data_val)
-    MetadataCatalog.get(f'fold_{fold}_val').thing_classes = ['nonTIL_stromal','sTIL','tumor_any','other']
-    MetadataCatalog.get(f'fold_{fold}_val').thing_colors = [(161,9,9),(239,222,0),(22,181,0),(0,32,193),(115,0,167)]
-
-    DatasetCatalog.register(f'test', data_test)
-    data = DatasetCatalog.get(f'test')
-    MetadataCatalog.get(f'test').thing_classes = ['nonTIL_stromal','sTIL','tumor_any','other']
-    MetadataCatalog.get(f'test').thing_colors = [(161,9,9),(239,222,0),(22,181,0),(0,32,193),(115,0,167)]
+    try:
+        DatasetCatalog.register(f'test', data_test)
+        MetadataCatalog.get(f'test').thing_classes = ['nonTIL_stromal','sTIL','tumor_any','other']
+        MetadataCatalog.get(f'test').thing_colors = [(161,9,9),(239,222,0),(22,181,0),(0,32,193),(115,0,167)]
+    except:
+        pass
 
     output_dir = os.path.join(args.output_dir, f'fold_{fold}')
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     
     register_coco_instances(f'fold_{fold}_train', {}, os.path.join(output_dir, f'train.json'), output_dir)
+    register_coco_instances(f'fold_{fold}_val', {}, os.path.join(output_dir, f'val.json'), output_dir)
+    register_coco_instances(f'test', {}, os.path.join(output_dir, f'test.json'), output_dir)
 
     return
 
