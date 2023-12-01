6aa2fac7f8c8438cf2fc910548b860e33cceb2f3
Aakash Rao <mrao.aakash@gmail.com>
2023-12-01 14:46:37 +0530

    added path

--- yolo/utils/all_models.py
+++ yolo/utils/all_models.py
@@ -30,7 +30,7 @@ axis_labels_dict = dict(zip(relevant_keys, axis_labels))
 plot_save_names_dict = dict(zip(relevant_keys, plot_save_names))
 
 
-def plot_model_individual(src_path, phase):
+def plot(src_path, phase):
     if not os.path.exists(src_path):
         print("File not found: ", src_path)
         raise FileNotFoundError(src_path)
@@ -54,51 +54,7 @@ def plot_model_individual(src_path, phase):
                     results = results[relevant_keys]
                     resultdict[dir] = results
             folds = len(resultdict.keys())
-            for key in relevant_keys:
-                plt.figure(figsize=(4,4))
-                x_key = 'epoch'
-                for dir in resultdict.keys():
-                    results = resultdict[dir]
-                    plt.plot(results[x_key], results[key], label=f'fold {dir[-1]}', linewidth=1)
-                plt.title(f'{plot_titles_dict[key]} for {folder} \n fold {dir[-1]}', fontsize=14, fontweight='bold')
-                plt.xlabel(axis_labels_dict[key], fontsize=14, fontweight='bold')
-                plt.ylabel(axis_labels_dict[key], fontsize=14, fontweight='bold')
-                if 'loss' in key:
-                    plt.ylim(0, max(results[key]))
-                else:
-                    plt.ylim(0, 1)
-                plt.legend(loc='best', fontsize=12)
-                plt.tight_layout()
-                plt.savefig(os.path.join(output_path, plot_save_names_dict[key] + ".png"), dpi=300)
-                plt.close()
-                # break
-                # finding mean across three models
-
-
-def plot_model_individual_with_collective(src_path, phase):
-    if not os.path.exists(src_path):
-        print("File not found: ", src_path)
-        raise FileNotFoundError(src_path)
-    elif len(os.listdir(src_path)) == 0:
-        print("Empty folder: ", src_path)
-        raise FileNotFoundError(src_path)
-    else:
-        for folder in model_list:
-            output_path = os.path.join(src_path, folder)
-            if not os.path.exists(output_path):
-                os.makedirs(output_path)
-            resultdict = {}
-            for dir in os.listdir(src_path):
-                if dir==folder:
-                    continue
-                elif folder not in dir:
-                    continue
-                else:
-                    results = pandas.read_csv(os.path.join(src_path,dir, "results.csv"))
-                    results = results.rename(columns=translation_dict)
-                    results = results[relevant_keys]
-                    resultdict[dir] = results
-            folds = len(resultdict.keys())
+            result_df = pandas.DataFrame(columns=relevant_keys)
             for key in relevant_keys:
                 if key == 'epoch':
                     continue
@@ -113,6 +69,7 @@ def plot_model_individual_with_collective(src_path, phase):
                 print(metric_sums)
                 sem = numpy.std(metric_sums, axis=0)#/numpy.sqrt(folds)
                 metric_sums = numpy.mean(metric_sums, axis=0)
+                result_df[key] = metric_sums
                 print(metric_sums)
                 print(sem)
 
@@ -132,6 +89,7 @@ def plot_model_individual_with_collective(src_path, phase):
                 plt.tight_layout()
                 plt.savefig(os.path.join(output_path, plot_save_names_dict[key] + "_mean.png"), dpi=300)
                 plt.close()
+            pd.DataFrame.to_csv(result_df, os.path.join(output_path, "results.csv"))
                 
             if phase == "testing":
                 return
@@ -142,5 +100,4 @@ if __name__ == "__main__":
     parser.add_argument("--src_path", type=str, default="results", help="path to the results folder")
     parser.add_argument("--phase", type=str, default="testing", help="phase to plot")
     args = parser.parse_args()
-    plot_model_individual(args.src_path, args.phase)
-    plot_model_individual_with_collective(args.src_path, args.phase)
+    plot(args.src_path, args.phase)
