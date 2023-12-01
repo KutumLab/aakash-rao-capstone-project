80bd6d8302826d18813ef0adce65e2a37af9a920
Aakash Rao <mrao.aakash@gmail.com>
2023-12-01 17:13:27 +0530

    added a bunch of stuff

---
+++ cascade_rcnn/configs/cascade-rcnn_x101_64x4d_fpn_20e_coco_fold_1.py
@@ -0,0 +1,48 @@
+# The new config inherits a base config to highlight the necessary modification
+base_path = '/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project'
+_base_ = f'{base_path}mmdetection_base/mmdetection/configs/cascade_rcnn/cascade-rcnn_x101_64x4d_fpn_20e_coco.py'
+
+# We also need to change the num_classes in head to match the dataset's annotation
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=1), mask_head=dict(num_classes=1)))
+
+# Modify dataset related settings
+data_root = ''
+metainfo = {
+    'classes': ('nonTIL_stromal', 'sTIL', 'tumor_any', 'other', ),
+}
+train_dataloader = dict(
+    batch_size=4,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file=f'{base_path}datasets/mmdetection/fold_1/train.json')
+)
+val_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file=f'{base_path}datasets/mmdetection/fold_1/val.json')
+)
+test_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        test_mode=True,
+        ann_file=f'{base_path}datasets/mmdetection/fold_1/test.json')
+)
+
+# Modify metric related settings
+val_evaluator = dict(ann_file=f'{base_path}datasets/mmdetection/fold_1/val.json')
+test_evaluator = dict(ann_file=f'{base_path}datasets/mmdetection/fold_1/test.json')
+
+train_cfg = dict(
+    type='EpochBasedTrainLoop',  # The training loop type. Refer to https://github.com/open-mmlab/mmengine/blob/main/mmengine/runner/loops.py
+    max_epochs=5,  # Maximum training epochs
+    val_interval=1)  # Validation intervals. Run validation every epoch.
+val_cfg = dict(type='ValLoop')  # The validation loop type
+test_cfg = dict(type='TestLoop')  # The testing loop type
+
+# We can use the pre-trained Mask RCNN model to obtain higher performance
+load_from = 'https://download.openmmlab.com/mmdetection/v2.0/cascade_rcnn/cascade_rcnn_x101_64x4d_fpn_20e_coco/cascade_rcnn_x101_64x4d_fpn_20e_coco_20200509_224357-051557b1.pth'
