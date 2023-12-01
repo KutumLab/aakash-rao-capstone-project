5f3c944c41e8629863eea19c15ce293122e204cb
Aakash Rao <mrao.aakash@gmail.com>
2023-12-01 18:21:47 +0530

    meh

--- faster_rcnn/plot.sh
+++ faster_rcnn/plot.sh
@@ -2,3 +2,5 @@
 
 python3 utils/json_gen.py \
     --path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron/faster_rcnn_R_50_DC5_1x_fold_1 \
+python3 utils/plot_model.py \
+    --path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron/faster_rcnn_R_50_DC5_1x_fold_1 \
--- faster_rcnn/utils/plot_model.py
+++ faster_rcnn/utils/plot_model.py
@@ -5,14 +5,19 @@ import numpy as np
 import pandas as pd
 
 
-def json_gen(path):
+def plot_model(path):
     # read json file
     info = pd.read_csv(os.path.join(path, 'metrics.csv'), index_col=False, header=0)
     info = info.sort_values(by=['iteration'])
     # remove nas
     info = info.dropna(axis=1)
-     print(info.head()['iteration'])
+    print(info.head()['iteration'])
 
+if __name__ == '__main__':
+    argparse = argparse.ArgumentParser()
+    argparse.add_argument('--path', help='path to json file')
+    args = argparse.parse_args()
+    plot_model(args.path)
 
 
     
