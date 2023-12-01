54dad954d492b694ed56fae4d1da1f17b93aca93
Aakash Rao <mrao.aakash@gmail.com>
2023-12-01 18:44:14 +0530

    meh

--- faster_rcnn/utils/plot_model.py
+++ faster_rcnn/utils/plot_model.py
@@ -44,7 +44,7 @@ def plot_model(path):
         plt.title(title_dict[col], fontsize=14, fontweight='bold')
         plt.xlabel(axis_dict[x_axis], fontsize=14, fontweight='bold')
         plt.ylabel(axis_dict[col], fontsize=14, fontweight='bold')
-        plt.xticks(np.arange(0, copy_info[x_axis].max(), 1000), fontsize=10)
+        plt.xticks(np.arange(0, copy_info[x_axis].max(), 3000), fontsize=10)
         plt.yticks(fontsize=10)
         plt.xlim(0, copy_info[x_axis].max()+1)
         plt.tight_layout()
