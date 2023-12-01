32d24b369e074c1fa74f7e71986335b19b8dc985
Aakash Rao <mrao.aakash@gmail.com>
2023-12-01 11:20:18 +0530

    updated info

--- retina_net/train_retina_net.sh
+++ retina_net/train_retina_net.sh
@@ -17,18 +17,80 @@ python3 train_retina_net.py \
     --fold 1 \
     --save_path $SAVE_PATH \
 
-python3 train_retina_net.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 2 \
-    --save_path $SAVE_PATH \
+# python3 train_retina_net.py \
+#     --data_path $DATA_PATH \
+#     --config_info $CONFIG \
+#     --max_iters $MAX_ITERS \
+#     --name $NAME \
+#     --fold 2 \
+#     --save_path $SAVE_PATH \
 
-python3 train_retina_net.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 3 \
-    --save_path $SAVE_PATH \
+# python3 train_retina_net.py \
+#     --data_path $DATA_PATH \
+#     --config_info $CONFIG \
+#     --max_iters $MAX_ITERS \
+#     --name $NAME \
+#     --fold 3 \
+#     --save_path $SAVE_PATH \
+
+
+# retinanet_R_101_FPN_3x
+
+# VARIABLE INFO
+# CONFIG=COCO-Detection/retinanet_R_101_FPN_3x.yaml
+# NAME=retinanet_R_101_FPN_3x
+
+# python3 train_retina_net.py \
+#     --data_path $DATA_PATH \
+#     --config_info $CONFIG \
+#     --max_iters $MAX_ITERS \
+#     --name $NAME \
+#     --fold 1 \
+#     --save_path $SAVE_PATH \
+
+# python3 train_retina_net.py \
+#     --data_path $DATA_PATH \
+#     --config_info $CONFIG \
+#     --max_iters $MAX_ITERS \
+#     --name $NAME \
+#     --fold 2 \
+#     --save_path $SAVE_PATH \
+
+# python3 train_retina_net.py \
+#     --data_path $DATA_PATH \
+#     --config_info $CONFIG \
+#     --max_iters $MAX_ITERS \
+#     --name $NAME \
+#     --fold 3 \
+#     --save_path $SAVE_PATH \
+
+
+# retinanet_R_50_FPN_3x
+
+# VARIABLE INFO
+# CONFIG=COCO-Detection/retinanet_R_50_FPN_3x.yaml
+# NAME=retinanet_R_50_FPN_3x
+
+# python3 train_retina_net.py \
+#     --data_path $DATA_PATH \
+#     --config_info $CONFIG \
+#     --max_iters $MAX_ITERS \
+#     --name $NAME \
+#     --fold 1 \
+#     --save_path $SAVE_PATH \
+
+# python3 train_retina_net.py \
+#     --data_path $DATA_PATH \
+#     --config_info $CONFIG \
+#     --max_iters $MAX_ITERS \
+#     --name $NAME \
+#     --fold 2 \
+#     --save_path $SAVE_PATH \
+
+# python3 train_retina_net.py \
+#     --data_path $DATA_PATH \
+#     --config_info $CONFIG \
+#     --max_iters $MAX_ITERS \
+#     --name $NAME \
+#     --fold 3 \
+#     --save_path $SAVE_PATH \
