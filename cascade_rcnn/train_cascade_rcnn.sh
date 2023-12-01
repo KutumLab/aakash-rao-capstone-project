0d5710c5053ea07c43e25d682b3f5707b907667c
Aakash Rao <mrao.aakash@gmail.com>
2023-12-01 17:17:35 +0530

    added docs

--- cascade_rcnn/train_cascade_rcnn.sh
+++ cascade_rcnn/train_cascade_rcnn.sh
@@ -2,5 +2,5 @@ WORK_DIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-ca
 CONFIG_PATH=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/cascade_rcnn/configs
 
 python3 ./mmdetection/tools/train.py \
-    $CONFIG_PATH/cascade_rcnn/cascade-rcnn_x101_64x4d_fpn_20e_coco_fold_1.py \
+    $CONFIG_PATH/cascade-rcnn_x101_64x4d_fpn_20e_coco_fold_1.py \
     --work-dir $WORK_DIR/cascade-rcnn_x101_64x4d_fpn_20e_coco_fold_1
