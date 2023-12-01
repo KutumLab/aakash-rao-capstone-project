4c332e31f81aa2ab1031c6aded600acf61d255ac
Aakash Rao <mrao.aakash@gmail.com>
2023-12-01 13:04:37 +0530

    added path

--- yolo/plot.sh
+++ yolo/plot.sh
@@ -1,3 +1,3 @@
 python3 ./utils/plot_single_figures.py \
-    --src_path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/yolo/logs/yolov5l \
+    --src_path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/yolo \
     --phase testing \
