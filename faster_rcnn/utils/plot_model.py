44ad6f3b8c76a5825271954dad784436cbdf4376
Aakash Rao <mrao.aakash@gmail.com>
2023-12-02 18:05:40 +0530

    added stuff

--- faster_rcnn/utils/plot_model.py
+++ faster_rcnn/utils/plot_model.py
@@ -49,7 +49,6 @@ def plot_model(path):
         plt.title(title_dict[col], fontsize=14, fontweight='bold')
         plt.xlabel(axis_dict[x_axis], fontsize=14, fontweight='bold')
         plt.ylabel(axis_dict[col], fontsize=14, fontweight='bold')
-        plt.xlim(0, copy_info[x_axis].max()+10)
         # check if mAP is a substring of the column name
         if 'map' in col.lower():
             plt.ylim(0, 1)
