d3ca17e66ff2e618c72a56b5fdb1813216779e6e
Aakash Rao <mrao.aakash@gmail.com>
2023-12-03 11:23:57 +0530

    added stuff

--- yolo/utils/diff.py
+++ yolo/utils/diff.py
@@ -1,7 +1,12 @@
 import os
+import numpy as np
 
 l1 = os.listdir('/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/yolov5/fold_1/train/images')
 l2 = os.listdir('/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/yolov5/test/images')
 
-diff = set(l1). difference(l2) 
-print(len(diff))
+l1 = np.array(l1)
+l2 = np.array(l2)
+
+intersection = np.intersect1d(l1, l2)
+print(intersection)
+print(len(intersection))
