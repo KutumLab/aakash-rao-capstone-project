5434c55092d3ce7b88b6acdec69eac594ea31ac6
Aakash Rao <mrao.aakash@gmail.com>
2023-12-03 11:39:10 +0530

    added stuff

--- yolo/utils/diff.py
+++ yolo/utils/diff.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-l1 = os.listdir('/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/yolov5/fold_1/train/images')
+l1 = os.listdir('/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/yolov5/fold_1/val/images')
 l2 = os.listdir('/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/yolov5/test/images')
 
 l1 = np.array(l1)
