eb77c7816017f87abe2df09325d906dff6439958
Aakash Rao <mrao.aakash@gmail.com>
2023-12-03 11:21:13 +0530

    added stuff

--- yolo/utils/diff.py
+++ yolo/utils/diff.py
@@ -1,7 +1,7 @@
 import os
 
 l1 = os.listdir('/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/yolov5/fold_1/train/images')
-l2 = os.listdir('/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/yolov5/fold_1/val/images')
+l2 = os.listdir('/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/yolov5/test/images')
 
 diff = set(l1). difference(l2) 
 print(diff)
