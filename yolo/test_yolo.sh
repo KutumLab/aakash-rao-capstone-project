4a193b7ef21e2a277c8a4d5c0883e808c92384b0
Aakash Rao <mrao.aakash@gmail.com>
2023-12-03 11:05:42 +0530

    added stuff

--- yolo/test_yolo.sh
+++ yolo/test_yolo.sh
@@ -15,6 +15,8 @@ BATCH_SIZE=16
 DEVICE=0
 SAVE_PERIOD=10
 PROJECT=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/yolo_test
+INPATH=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/yolo
+
 
 echo "Training YOLOv5s"
 echo $input
@@ -28,16 +30,16 @@ echo $BATCH_SIZE
 echo $DEVICE
 echo $SAVE_PERIOD
 
-python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_1 --weights $PROJECT/yolov5m-fold_1/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5m-fold_1_test --task test --save-txt --save-conf --save-hybrid --save-json
-# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_2 --weights $PROJECT/yolov5m-fold_2/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5m-fold_2_test --task test --save-txt --save-conf --save-hybrid --save-json
-# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_3 --weights $PROJECT/yolov5m-fold_3/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5m-fold_3_test --task test --save-txt --save-conf --save-hybrid --save-json
+python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_1 --weights $INPATH/yolov5m-fold_1/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5m-fold_1_test --task test --save-txt --save-conf --save-hybrid --save-json
+# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_2 --weights $INPATH/yolov5m-fold_2/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5m-fold_2_test --task test --save-txt --save-conf --save-hybrid --save-json
+# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_3 --weights $INPATH/yolov5m-fold_3/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5m-fold_3_test --task test --save-txt --save-conf --save-hybrid --save-json
 
 
-# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_1 --weights $PROJECT/yolov5l-fold_1/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5l-fold_1_test --task test --save-txt --save-conf --save-hybrid --save-json
-# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_2 --weights $PROJECT/yolov5l-fold_2/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5l-fold_2_test --task test --save-txt --save-conf --save-hybrid --save-json
-# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_3 --weights $PROJECT/yolov5l-fold_3/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5l-fold_3_test --task test --save-txt --save-conf --save-hybrid --save-json
+# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_1 --weights $INPATH/yolov5l-fold_1/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5l-fold_1_test --task test --save-txt --save-conf --save-hybrid --save-json
+# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_2 --weights $INPATH/yolov5l-fold_2/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5l-fold_2_test --task test --save-txt --save-conf --save-hybrid --save-json
+# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_3 --weights $INPATH/yolov5l-fold_3/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5l-fold_3_test --task test --save-txt --save-conf --save-hybrid --save-json
 
 
-# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_1 --weights $PROJECT/yolov5x-fold_1/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5x-fold_1_test --task test --save-txt --save-conf --save-hybrid --save-json
-# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_2 --weights $PROJECT/yolov5x-fold_2/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5x-fold_2_test --task test --save-txt --save-conf --save-hybrid --save-json
-# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_3 --weights $PROJECT/yolov5x-fold_3/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5x-fold_3_test --task test --save-txt --save-conf --save-hybrid --save-json
+# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_1 --weights $INPATH/yolov5x-fold_1/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5x-fold_1_test --task test --save-txt --save-conf --save-hybrid --save-json
+# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_2 --weights $INPATH/yolov5x-fold_2/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5x-fold_2_test --task test --save-txt --save-conf --save-hybrid --save-json
+# python ./yolov5/val.py --img $IMG_SIZE --data $YAML_FOLD_3 --weights $INPATH/yolov5x-fold_3/weights/best.pt --device $DEVICE --batch-size $BATCH_SIZE --project $PROJECT --name yolov5x-fold_3_test --task test --save-txt --save-conf --save-hybrid --save-json
