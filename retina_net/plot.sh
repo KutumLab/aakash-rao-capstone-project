c42bba9f662053f4056a92a22961bf7a0f565f1e
Aakash Rao <mrao.aakash@gmail.com>
2023-12-04 12:35:53 +0530

    added path info

--- retina_net/plot.sh
+++ retina_net/plot.sh
@@ -2,16 +2,16 @@
 BASEPATH=/media/chs.gpu/19b31863-d2db-44c2-b407-9a4ffbebcbd1/DATA/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron
 
 
-MODEL=retinanet_R_101_FPN_3x
-for FOLD in 1 2 3 
-do
-    python3 utils/json_gen.py \
-        --path $BASEPATH/"$MODEL"_fold_${FOLD}
+# MODEL=retinanet_R_101_FPN_3x
+# for FOLD in 1 2 3 
+# do
+#     python3 utils/json_gen.py \
+#         --path $BASEPATH/"$MODEL"_fold_${FOLD}
 
-    python3 utils/plot_model.py \
-        --path $BASEPATH/"$MODEL"_fold_${FOLD} \
-        --name "ResNet101 at 1x" 
-done
+#     python3 utils/plot_model.py \
+#         --path $BASEPATH/"$MODEL"_fold_${FOLD} \
+#         --name "ResNet101 at 1x" 
+# done
 
 
 
