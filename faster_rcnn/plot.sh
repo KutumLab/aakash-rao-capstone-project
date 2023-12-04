8d1fc8931875fab507b259e48d3639abc2d87cf6
Aakash Rao <mrao.aakash@gmail.com>
2023-12-04 08:47:21 +0530

    added stuff

--- faster_rcnn/plot.sh
+++ faster_rcnn/plot.sh
@@ -6,5 +6,5 @@ do
 
     python3 utils/plot_model.py \
         --path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron/faster_rcnn_R_50_DC5_1x_fold_${FOLD} \
-        --name ResNet50+DC5
+        --name "ResNet50+DC5"
 done
