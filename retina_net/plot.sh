cf65777977f5dcd27956458cecdc984603360c7d
Aakash Rao <mrao.aakash@gmail.com>
2023-12-04 12:23:17 +0530

    added path info

--- retina_net/plot.sh
+++ retina_net/plot.sh
@@ -14,24 +14,24 @@ do
 done
 
 
-# MODEL=retinanet_R_50_FPN_1x
-# for FOLD in 1 2 3 
-# do
-#     python3 utils/json_gen.py \
-#         --path $BASEPATH/"$MODEL"_fold_${FOLD}
+MODEL=retinanet_R_50_FPN_1x
+for FOLD in 1 2 3 
+do
+    python3 utils/json_gen.py \
+        --path $BASEPATH/"$MODEL"_fold_${FOLD}
 
-#     python3 utils/plot_model.py \
-#         --path $BASEPATH/"$MODEL"_fold_${FOLD} \
-#         --name "ResNet50+FPN at 3x" 
-# done
+    # python3 utils/plot_model.py \
+    #     --path $BASEPATH/"$MODEL"_fold_${FOLD} \
+    #     --name "ResNet50+FPN at 3x" 
+done
 
-# MODEL=retinanet_R_50_FPN_3x
-# for FOLD in 1 2 3 
-# do
-#     python3 utils/json_gen.py \
-#         --path $BASEPATH/"$MODEL""_fold_${FOLD}
+MODEL=retinanet_R_50_FPN_3x
+for FOLD in 1 2 3 
+do
+    python3 utils/json_gen.py \
+        --path $BASEPATH/"$MODEL"_fold_${FOLD}
 
-#     python3 utils/plot_model.py \
-#         --path $BASEPATH/"$MODEL"_fold_${FOLD} \
-#         --name "ResNet101+FPN at 3x" 
-# done
+    # python3 utils/plot_model.py \
+    #     --path $BASEPATH/"$MODEL"_fold_${FOLD} \
+    #     --name "ResNet101+FPN at 3x" 
+done
