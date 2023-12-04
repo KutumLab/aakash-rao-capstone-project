f57ed2b43c3d61f8eede41220f9df47eec84c01c
Aakash Rao <mrao.aakash@gmail.com>
2023-12-04 12:20:40 +0530

    added path info

--- faster_rcnn/train_faster_rcnn.sh
+++ faster_rcnn/train_faster_rcnn.sh
@@ -5,7 +5,7 @@ SAVE_PATH=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-c
 MAX_ITERS=15000
 
 
-# VARIABLE INFO [DONE]
+# # VARIABLE INFO [DONE]
 # CONFIG=COCO-Detection/faster_rcnn_R_50_DC5_1x.yaml
 # NAME=faster_rcnn_R_50_DC5_1x
 
@@ -34,36 +34,8 @@ MAX_ITERS=15000
 #     --save_path $SAVE_PATH \
 
 
-# VARIABLE INFO faster_rcnn_R_101_C4_3x
-CONFIG=COCO-Detection/faster_rcnn_R_101_C4_3x.yaml
-NAME=faster_rcnn_R_101_C4_3x
 
-python3 train_faster_rcnn.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 1 \
-    --save_path $SAVE_PATH \
-
-python3 train_faster_rcnn.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 2 \
-    --save_path $SAVE_PATH \
-
-python3 train_faster_rcnn.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 3 \
-    --save_path $SAVE_PATH \
-
-
-# VARIABLE INFO faster_rcnn_R_101_DC5_3x [DONE]
+# # VARIABLE INFO faster_rcnn_R_101_DC5_3x [DONE]
 # CONFIG=COCO-Detection/faster_rcnn_R_101_DC5_3x.yaml
 # NAME=faster_rcnn_R_101_DC5_3x
 
@@ -93,99 +65,9 @@ python3 train_faster_rcnn.py \
 
 
 
-# VARIABLE INFO faster_rcnn_R_50_C4_1x
-CONFIG=COCO-Detection/faster_rcnn_R_50_C4_1x.yaml
-NAME=faster_rcnn_R_50_C4_1x
-
-python3 train_faster_rcnn.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 1 \
-    --save_path $SAVE_PATH \
-
-python3 train_faster_rcnn.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 2 \
-    --save_path $SAVE_PATH \
-
-python3 train_faster_rcnn.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 3 \
-    --save_path $SAVE_PATH \
-
-
-
-
-# VARIABLE INFO faster_rcnn_R_50_C4_3x
-CONFIG=COCO-Detection/faster_rcnn_R_50_C4_3x.yaml
-NAME=faster_rcnn_R_50_C4_3x
-
-python3 train_faster_rcnn.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 1 \
-    --save_path $SAVE_PATH \
-
-python3 train_faster_rcnn.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 2 \
-    --save_path $SAVE_PATH \
-
-python3 train_faster_rcnn.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 3 \
-    --save_path $SAVE_PATH \
-
-
-
-# VARIABLE INFO faster_rcnn_R_50_DC5_3x
-CONFIG=COCO-Detection/faster_rcnn_R_50_DC5_3x.yaml
-NAME=faster_rcnn_R_50_DC5_3x
-
-python3 train_faster_rcnn.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 1 \
-    --save_path $SAVE_PATH \
-
-python3 train_faster_rcnn.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 2 \
-    --save_path $SAVE_PATH \
-
-python3 train_faster_rcnn.py \
-    --data_path $DATA_PATH \
-    --config_info $CONFIG \
-    --max_iters $MAX_ITERS \
-    --name $NAME \
-    --fold 3 \
-    --save_path $SAVE_PATH \
-
-
-# # VARIABLE INFO rpn_R_50_C4_1x
-# CONFIG=COCO-Detection/rpn_R_50_C4_1x.yaml
-# NAME=rpn_R_50_C4_1x
+# # VARIABLE INFO faster_rcnn_R_50_C4_1x [DONE]
+# CONFIG=COCO-Detection/faster_rcnn_R_50_C4_1x.yaml
+# NAME=faster_rcnn_R_50_C4_1x
 
 # python3 train_faster_rcnn.py \
 #     --data_path $DATA_PATH \
@@ -213,9 +95,10 @@ python3 train_faster_rcnn.py \
 
 
 
-# # VARIABLE INFO rpn_R_50_FPN_1x
-# CONFIG=COCO-Detection/rpn_R_50_FPN_1x.yaml
-# NAME=rpn_R_50_FPN_1x
+
+# # VARIABLE INFO faster_rcnn_R_50_C4_3x [DONE]
+# CONFIG=COCO-Detection/faster_rcnn_R_50_C4_3x.yaml
+# NAME=faster_rcnn_R_50_C4_3x
 
 # python3 train_faster_rcnn.py \
 #     --data_path $DATA_PATH \
@@ -240,3 +123,189 @@ python3 train_faster_rcnn.py \
 #     --name $NAME \
 #     --fold 3 \
 #     --save_path $SAVE_PATH \
+
+
+
+# # VARIABLE INFO faster_rcnn_R_50_DC5_3x [DONE]
+# CONFIG=COCO-Detection/faster_rcnn_R_50_DC5_3x.yaml
+# NAME=faster_rcnn_R_50_DC5_3x
+
+# python3 train_faster_rcnn.py \
+#     --data_path $DATA_PATH \
+#     --config_info $CONFIG \
+#     --max_iters $MAX_ITERS \
+#     --name $NAME \
+#     --fold 1 \
+#     --save_path $SAVE_PATH \
+
+# python3 train_faster_rcnn.py \
+#     --data_path $DATA_PATH \
+#     --config_info $CONFIG \
+#     --max_iters $MAX_ITERS \
+#     --name $NAME \
+#     --fold 2 \
+#     --save_path $SAVE_PATH \
+
+# python3 train_faster_rcnn.py \
+#     --data_path $DATA_PATH \
+#     --config_info $CONFIG \
+#     --max_iters $MAX_ITERS \
+#     --name $NAME \
+#     --fold 3 \
+#     --save_path $SAVE_PATH \
+
+
+
+# SEMI-CONSTANT INFO
+DATA_PATH=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/detectron
+SAVE_PATH=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs
+MAX_ITERS=15000
+
+# VARIABLE INFO faster_rcnn_X_101_FPN_3x [DONE]
+CONFIG=COCO-Detection/faster_rcnn_X_101_FPN_3x.yaml
+NAME=faster_rcnn_X_101_FPN_3x
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 1 \
+    --save_path $SAVE_PATH \
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 2 \
+    --save_path $SAVE_PATH \
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 3 \
+    --save_path $SAVE_PATH \
+
+
+
+# VARIABLE INFO faster_rcnn_R_101_FPN_3x [DONE]
+CONFIG=COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml
+NAME=faster_rcnn_R_101_FPN_3x
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 1 \
+    --save_path $SAVE_PATH \
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 2 \
+    --save_path $SAVE_PATH \
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 3 \
+    --save_path $SAVE_PATH \
+
+
+
+# VARIABLE INFO faster_rcnn_R_101_C4_3x [DONE]
+CONFIG=COCO-Detection/faster_rcnn_R_101_C4_3x.yaml
+NAME=faster_rcnn_R_101_C4_3x
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 1 \
+    --save_path $SAVE_PATH \
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 2 \
+    --save_path $SAVE_PATH \
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 3 \
+    --save_path $SAVE_PATH \
+
+
+
+# VARIABLE INFO faster_rcnn_R_50_FPN_3x [DONE]
+CONFIG=COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml
+NAME=faster_rcnn_R_50_FPN_3x
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 1 \
+    --save_path $SAVE_PATH \
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 2 \
+    --save_path $SAVE_PATH \
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 3 \
+    --save_path $SAVE_PATH \
+
+
+
+
+# VARIABLE INFO faster_rcnn_R_50_FPN_1x [DONE]
+CONFIG=COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml
+NAME=faster_rcnn_R_50_FPN_1x
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 1 \
+    --save_path $SAVE_PATH \
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 2 \
+    --save_path $SAVE_PATH \
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 3 \
+    --save_path $SAVE_PATH \
--- retina_net/plot.sh
+++ retina_net/plot.sh
@@ -1,30 +1,37 @@
 # for loop
+BASEPATH=/media/chs.gpu/19b31863-d2db-44c2-b407-9a4ffbebcbd1/DATA/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron
+
+
+MODEL=retinanet_R_101_FPN_3x
 for FOLD in 1 2 3 
 do
     python3 utils/json_gen.py \
-        --path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron/retinanet_R_50_FPN_1x_fold_${FOLD}
+        --path $BASEPATH/"$MODEL"_fold_${FOLD}
 
-    python3 utils/plot_model.py \
-        --path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron/retinanet_R_50_FPN_1x_fold_${FOLD} \
-        --name "ResNet50+FPN at 1x" 
+    # python3 utils/plot_model.py \
+    #     --path $BASEPATH/"$MODEL"_fold_${FOLD} \
+    #     --name "ResNet50+FPN at 1x" 
 done
 
-for FOLD in 1 2 3 
-do
-    python3 utils/json_gen.py \
-        --path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron/retinanet_R_50_FPN_3x_fold_${FOLD}
 
-    python3 utils/plot_model.py \
-        --path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron/retinanet_R_50_FPN_3x_fold_${FOLD} \
-        --name "ResNet50+FPN at 3x" 
-done
+# MODEL=retinanet_R_50_FPN_1x
+# for FOLD in 1 2 3 
+# do
+#     python3 utils/json_gen.py \
+#         --path $BASEPATH/"$MODEL"_fold_${FOLD}
 
-for FOLD in 1 2 3 
-do
-    python3 utils/json_gen.py \
-        --path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron/retinanet_R_101_FPN_3x_fold_${FOLD}
+#     python3 utils/plot_model.py \
+#         --path $BASEPATH/"$MODEL"_fold_${FOLD} \
+#         --name "ResNet50+FPN at 3x" 
+# done
 
-    python3 utils/plot_model.py \
-        --path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron/retinanet_R_101_FPN_3x_fold_${FOLD} \
-        --name "ResNet101+FPN at 3x" 
-done
+# MODEL=retinanet_R_50_FPN_3x
+# for FOLD in 1 2 3 
+# do
+#     python3 utils/json_gen.py \
+#         --path $BASEPATH/"$MODEL""_fold_${FOLD}
+
+#     python3 utils/plot_model.py \
+#         --path $BASEPATH/"$MODEL"_fold_${FOLD} \
+#         --name "ResNet101+FPN at 3x" 
+# done
