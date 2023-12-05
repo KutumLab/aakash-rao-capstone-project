d7b3293eca358bdccd92a327bb19fcfe4cd3563c
Aakash Rao <mrao.aakash@gmail.com>
2023-12-05 20:23:35 +0530

    added path info

--- general/generate_all_model_stats.py
+++ general/generate_all_model_stats.py
@@ -39,9 +39,21 @@ def gen_stat(src_path):
 
     pass
 
+best_from_all = ['faster_rcnn_R_50_C4_3x', 'retina_net_R_101_FPN_3x', 'yolov5m']
+
+def grp_plot(src_path):
+    mean_frcnn = pd.read_csv(os.path.join(src_path, 'faster_rcnn', 'csvs', 'mean_faster_rcnn_R_50_C4_3x.csv'), header=0)
+    mean_retinanet = pd.read_csv(os.path.join(src_path, 'retina_net', 'csvs', 'mean_retina_net_R_101_FPN_3x.csv'), header=0)
+    mean_yolo = pd.read_csv(os.path.join(src_path, 'yolo', 'csvs', 'mean_yolov5m.csv'), header=0)
+    std_frcnn = pd.read_csv(os.path.join(src_path, 'faster_rcnn', 'csvs', 'std_faster_rcnn_R_50_C4_3x.csv'), header=0)
+    std_retinanet = pd.read_csv(os.path.join(src_path, 'retina_net', 'csvs', 'std_retina_net_R_101_FPN_3x.csv'), header=0)
+    std_yolo = pd.read_csv(os.path.join(src_path, 'yolo', 'csvs', 'std_yolov5m.csv'), header=0)
+    pass
+
 if __name__ == '__main__':
     argparse = argparse.ArgumentParser()
     argparse.add_argument('-s','--src_path', type=str, default='data')
     args = argparse.parse_args()
-    gen_stat(args.src_path)
+    # gen_stat(args.src_path)
+    grp_plot(args.src_path)
     pass
