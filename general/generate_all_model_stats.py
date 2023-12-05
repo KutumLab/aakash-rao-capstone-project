9c9b91251763b61449ad033969bdab34fd948fe7
Aakash Rao <mrao.aakash@gmail.com>
2023-12-05 19:50:13 +0530

    added path info

---
+++ general/generate_all_model_stats.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import os
+import argparse
+
+models = ['faster_rcnn', 'retina_net', 'yolo']
+dict_oof = {'yolo':['mAP_'], 'retina_net':['AP'], 'faster_rcnn':['AP']}
+def gen_stat(src_path):
+    models_list = []
+    ap50_list = []
+    ap5095_list = []
+    for model in os.listdir(src_path):
+        model_path = os.path.join(src_path, model, "csvs")
+
+
+        for submodel in os.listdir(model_path):
+            mean_df = pd.read_csv(os.path.join(model_path, submodel, f'mean_{submodel}.csv'), header=0)
+            std_df = pd.read_csv(os.path.join(model_path, submodel, f'std_{submodel}.csv'), header=0)
+            models_list.append(model)
+            ap50 = mean_df[f'{dict_oof[model][0]}50'].values.to_numpy()
+            # appending max value
+            ap50_list.append(np.max(ap50))
+            ap5095 = mean_df[f'{dict_oof[model][0]}5095'].values.to_numpy()
+            # appending max value
+            ap5095_list.append(np.max(ap5095))
+    print(models_list)
+    print(ap50_list)
+    print(ap5095_list)
+    
+
+
+    pass
+
+if __name__ == '__main__':
+    argparse = argparse.ArgumentParser()
+    argparse.add_argument('-s','--src_path', type=str, default='data')
+    pass
---
+++ general/runmodelstats.sh
@@ -0,0 +1,2 @@
+python3 generate_all_model_stats.py \
+    -s /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/ 
