ed2f3e0c037773f89ddbc42e17c5b3141f7f3827
Aakash Rao <mrao.aakash@gmail.com>
2023-12-05 20:44:46 +0530

    added path info

--- general/generate_all_model_stats.py
+++ general/generate_all_model_stats.py
@@ -72,23 +72,20 @@ def grp_plot(src_path):
     std_yolo_5095 = std_yolo['mAP_50_95'].dropna(how='all').values
 
 
-    # bringing all to same length
-    mean_yolo_50 = mean_yolo_50
-    mean_yolo_5095 = mean_yolo_5095
-    std_yolo_50 = std_yolo_50
-    std_yolo_5095 = std_yolo_5095
-    # mean_frcnn_50 = mean_frcnn_50[:,:len(mean_frcnn_50)//len(mean_yolo_50)]
 
-    print(mean_frcnn_50.shape)
-    print(mean_yolo_50.shape)
-    print(mean_retinanet_50.shape)
-    print(std_frcnn_50.shape)
-    print(std_yolo_50.shape)
-    print(std_retinanet_50.shape)
-
-
-    # plt.figure(figsize=(10, 5))
-    # plt.plot
+    plt.figure(figsize=(10, 5))
+    plt.plot(mean_frcnn_50, label='faster_rcnn', color='r')
+    plt.plot(mean_retinanet_50, label='retinanet', color='b')
+    plt.plot(mean_yolo_50, label='yolo', color='g')
+    plt.fill_between(np.arange(len(mean_frcnn_50)), mean_frcnn_50-std_frcnn_50, mean_frcnn_50+std_frcnn_50, alpha=0.3, color='r')
+    plt.fill_between(np.arange(len(mean_retinanet_50)), mean_retinanet_50-std_retinanet_50, mean_retinanet_50+std_retinanet_50, alpha=0.3, color='b')
+    plt.fill_between(np.arange(len(mean_yolo_50)), mean_yolo_50-std_yolo_50, mean_yolo_50+std_yolo_50, alpha=0.3, color='g')
+    plt.xticks(np.arange(len(mean_frcnn_50)), np.arange(1, len(mean_frcnn_50)+1))
+    plt.xlabel('Epoch')
+    plt.ylabel('AP50')
+    plt.legend()
+    plt.savefig('ap50.png')
+    plt.close()
 
 
 
