2c0ff39a6248975a930a1b78e194ad97d03fb32a
Aakash Rao <mrao.aakash@gmail.com>
2023-12-05 16:33:55 +0530

    added path info

---
+++ faster_rcnn/utils/generate_csv.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+import argparse
+
+original_titles = []
+translation = []
+relevant = []
+rename = {}
+
+
+def generate_csvs(data_dir, output_dir):
+    
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    for folder in os.listdir(data_dir):
+        foldr_split = folder.split('_')
+        net_name = foldr_split[:-2].join('_')
+        fold_num = foldr_split[-2:]
+        print(net_name, fold_num)
+    pass
+
+def mean_and_std_fold(data_dir, output_dir):
+    
+
+    pass
+
+if __name__ == '__main__':
+    argparse = argparse.ArgumentParser()
+    argparse.add_argument('-d','--data_dir', type=str, default='data')
+    argparse.add_argument('-o','--output_dir', type=str, default='data')
+
+    args = argparse.parse_args()
+    generate_csvs(args.data_dir, args.output_dir)
+    # mean_and_std_fold(args.output_dir, args.output_dir)
