98e7dd0c9deb3be515b6cff6d70e60565cc2b3d7
Aakash Rao <mrao.aakash@gmail.com>
2023-12-05 15:26:57 +0530

    added path info

--- yolo/plot.sh
+++ yolo/plot.sh
@@ -1,5 +1,5 @@
 INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/yolo
 OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/yolo/csvs
-python3 yolo/utils/generate_csvs.py \
+python3 utils/generate_csvs.py \
     -d $INDIR \
     -o $OUTDIR
