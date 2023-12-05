6c4c7edfed1069f40c57ce2edfb2ef2de3c4865a
Aakash Rao <mrao.aakash@gmail.com>
2023-12-05 13:46:18 +0530

    added path info

--- general/runstats.sh
+++ general/runstats.sh
@@ -1,4 +1,8 @@
+# python3 NuCLS_statistics.py \
+#     -i /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/rgb \
+#     -m /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/csv \
+#     -p /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/NuCLS_Stastistics \
+
 python3 NuCLS_statistics.py \
-    -i /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/rgb \
-    -m /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/csv \
-    -p /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/NuCLS_Stastistics \
+    -i /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/NuCLS_Stastistics \
+    -p /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/NuCLS_Stastistics/plots \
---
+++ general/stat_plot_nucls.py
@@ -0,0 +1,32 @@
+import os 
+import numpy as np
+import pandas as pd
+import cv2
+import argparse
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+import time
+import json
+
+
+def plot_stats(image_dir, plot_dir):
+    class_stats = pd.read_csv(os.path.join(image_dir, 'class_stats.csv'), header=0)
+    num_classes_per_image = pd.read_csv(os.path.join(image_dir, 'num_classes_per_image.csv'), header=0)
+    print(class_stats)
+    print(num_classes_per_image)
+    
+
+
+                
+
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser(description='Data Formatter')
+    argparser.add_argument('-i', '--image_dir', required=True, help='image directory')
+    argparser.add_argument('-p', '--plot_directory', required=True, help='save directory')
+    args = argparser.parse_args()
+
+    print("Generating plots for NuCLS Dataset Statistics...")
+    plot_stats(args.image_dir, args.mask_dir, args.plot_directory)
