3f1f7a2b8895933c45ec484c90fb55b85ccae6fe
Aakash Rao <mrao.aakash@gmail.com>
2023-12-05 16:34:38 +0530

    added path info

--- faster_rcnn/plot.sh
+++ faster_rcnn/plot.sh
@@ -1,6 +1,6 @@
-python3 utils/json_gen.py \
-    --path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron
+# python3 utils/json_gen.py \
+#     --path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron
 
-    
-python3 utils/generate_csv.py.py \
+
+python3 utils/generate_csv.py \
     --path /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron
