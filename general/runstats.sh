d1da7f92eaeeb9de10bf3eea89db6cc5cd97b804
Aakash Rao <mrao.aakash@gmail.com>
2023-12-05 13:28:46 +0530

    added path info

---
+++ general/runstats.sh
@@ -0,0 +1,4 @@
+python3 NuCLS_statistics.py \
+    -i /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/rgb \
+    -m /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/csv \
+    -p /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/NuCLS_Stastistics \
