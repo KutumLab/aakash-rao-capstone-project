8202b1fc6186df720129854d1a5d9bb9b6b9fba2
Aakash Rao <mrao.aakash@gmail.com>
2023-12-05 17:16:45 +0530

    added path info

--- retina_net/plot.sh
+++ retina_net/plot.sh
@@ -11,8 +11,8 @@ python3 utils/generate_csv.py \
 
 
 
-# INDIR=/media/chs.gpu/19b31863-d2db-44c2-b407-9a4ffbebcbd1/DATA/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/csvs
-# OUTDIR=/media/chs.gpu/19b31863-d2db-44c2-b407-9a4ffbebcbd1/DATA/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/plots
-# python3 utils/plot_faster_rcnn.py \
-#     -d $INDIR \
-#     -o $OUTDIR
+INDIR=/media/chs.gpu/19b31863-d2db-44c2-b407-9a4ffbebcbd1/DATA/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/csvs
+OUTDIR=/media/chs.gpu/19b31863-d2db-44c2-b407-9a4ffbebcbd1/DATA/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/plots
+python3 utils/plot_faster_rcnn.py \
+    -d $INDIR \
+    -o $OUTDIR
