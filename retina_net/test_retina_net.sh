1d8a5eccf50b9984725b7941d4ba29df281d2b7b
Aakash Rao <mrao.aakash@gmail.com>
2023-12-06 15:04:46 +0530

    added path info

---
+++ retina_net/test_retina_net.sh
@@ -0,0 +1,36 @@
+
+# SEMI-CONSTANT INFO
+DATA_PATH=/media/chs.gpu/19b31863-d2db-44c2-b407-9a4ffbebcbd1/DATA/research-cancerPathology/aakash-rao-capstone-project/datasets/detectron
+SAVE_PATH=/media/chs.gpu/19b31863-d2db-44c2-b407-9a4ffbebcbd1/DATA/research-cancerPathology/aakash-rao-capstone-project/outputs
+MAX_ITERS=10000
+
+
+# VARIABLE INFO
+CONFIG=COCO-Detection/retinanet_R_101_FPN_3x.yaml
+NAME=retinanet_R_101_FPN_3x
+
+python3 train_retina_net.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 1 \
+    --save_path $SAVE_PATH \
+
+# python3 train_retina_net.py \
+#     --data_path $DATA_PATH \
+#     --config_info $CONFIG \
+#     --max_iters $MAX_ITERS \
+#     --name $NAME \
+#     --fold 2 \
+#     --save_path $SAVE_PATH \
+
+# python3 train_retina_net.py \
+#     --data_path $DATA_PATH \
+#     --config_info $CONFIG \
+#     --max_iters $MAX_ITERS \
+#     --name $NAME \
+#     --fold 3 \
+#     --save_path $SAVE_PATH \
+
+
