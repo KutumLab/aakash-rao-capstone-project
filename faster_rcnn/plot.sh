4e7b33b088821a447c5b7db73ce069b56bf0eda4
Aakash Rao <mrao.aakash@gmail.com>
2023-12-08 10:51:35 +0530

    added

--- faster_rcnn/plot.sh
+++ faster_rcnn/plot.sh
@@ -3,16 +3,16 @@ python3 utils/json_gen.py \
     --path $INDIR 
 
 
-INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron
-OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/faster_rcnn/csvs
-python3 utils/generate_csv.py \
-    -d $INDIR \
-    -o $OUTDIR \
+# INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron
+# OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/faster_rcnn/csvs
+# python3 utils/generate_csv.py \
+#     -d $INDIR \
+#     -o $OUTDIR \
 
 
 
-INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/faster_rcnn/csvs
-OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/faster_rcnn/plots
-python3 utils/plot_faster_rcnn.py \
-    -d $INDIR \
-    -o $OUTDIR
+# INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/faster_rcnn/csvs
+# OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/faster_rcnn/plots
+# python3 utils/plot_faster_rcnn.py \
+#     -d $INDIR \
+#     -o $OUTDIR
