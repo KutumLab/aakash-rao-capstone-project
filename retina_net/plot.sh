305ba50f6461621a42767a4afd38abfd663a326c
Aakash Rao <mrao.aakash@gmail.com>
2023-12-11 13:01:05 +0530

    idk tbh

--- retina_net/plot.sh
+++ retina_net/plot.sh
@@ -1,26 +1,26 @@
-INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron
-python3 utils/json_gen.py \
-    --path $INDIR 
+# INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron
+# python3 utils/json_gen.py \
+#     --path $INDIR 
 
 
-INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron
-OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/csvs
-python3 utils/generate_csv.py \
-    -d $INDIR \
-    -o $OUTDIR \
-
-
-
-INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/csvs
-OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/plots
-python3 utils/plot_retina_net.py \
-    -d $INDIR \
-    -o $OUTDIR
-
-# OLDDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/detectron/retina_net/csvs
-# NEWDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/csvs
-# OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/comparative
-# python3 utils/comparative.py \
-#     -d $OLDDIR \
+# INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/detectron
+# OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/csvs
+# python3 utils/generate_csv.py \
+#     -d $INDIR \
 #     -o $OUTDIR \
-#     -n $NEWDIR 
+
+
+
+# INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/csvs
+# OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/plots
+# python3 utils/plot_retina_net.py \
+#     -d $INDIR \
+#     -o $OUTDIR
+
+OLDDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/detectron/retina_net/csvs
+NEWDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/csvs
+OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/comparative
+python3 utils/comparative.py \
+    -d $OLDDIR \
+    -o $OUTDIR \
+    -n $NEWDIR 
