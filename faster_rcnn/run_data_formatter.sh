0f234d0ee5427e96e3b8bfb5d35ae01a650a371d
Aakash Rao <mrao.aakash@gmail.com>
2023-12-11 14:27:46 +0530

    added stuff

--- faster_rcnn/run_data_formatter.sh
+++ faster_rcnn/run_data_formatter.sh
@@ -1,4 +1,4 @@
-# $BASE_PATH=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project
+$BASE_PATH=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project
 
 # multiline run data formatter
 # python3 utils/data_formatter.py \
