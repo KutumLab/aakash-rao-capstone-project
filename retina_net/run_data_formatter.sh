472012b8c684a0aed0325da3df8c75684869ccb4
Aakash Rao <mrao.aakash@gmail.com>
2023-12-11 16:18:59 +0530

    added stuff

--- retina_net/run_data_formatter.sh
+++ retina_net/run_data_formatter.sh
@@ -8,3 +8,14 @@ python3 utils/data_formatter.py \
     -p run \
     -f 3 \
     --seed 42 \
+
+
+python3 utils/data_formatter.py \
+    -i /media/chs.gpu/19b31863-d2db-44c2-b407-9a4ffbebcbd1/DATA/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/rgb \
+    -m /media/chs.gpu/19b31863-d2db-44c2-b407-9a4ffbebcbd1/DATA/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/csv \
+    -s /media/chs.gpu/19b31863-d2db-44c2-b407-9a4ffbebcbd1/DATA/research-cancerPathology/aakash-rao-capstone-project/datasets/detectron_three_class \
+    -mp /media/chs.gpu/19b31863-d2db-44c2-b407-9a4ffbebcbd1/DATA/research-cancerPathology/aakash-rao-capstone-project/hypothesis_1/output/models/Xception_three_class/Xception_three_class.h5 \
+    -p run \
+    -f 3 \
+    -v three_class \
+    --seed 42 \
