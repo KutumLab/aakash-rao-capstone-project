299fff568317e18d67d6dd23dc4268209c0f9763
Aakash Rao <mrao.aakash@gmail.com>
2023-12-11 14:30:40 +0530

    added stuff

--- faster_rcnn/run_data_formatter.sh
+++ faster_rcnn/run_data_formatter.sh
@@ -2,30 +2,28 @@
 $BASE_PATH=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project
 # multiline run data formatter
 # python3 utils/data_formatter.py \
-#     -i $BASE_PATH/datasets/master/NuCLSEvalSet/rgb \
-#     -m $BASE_PATH/datasets/master/NuCLSEvalSet/csv \
-#     -s $BASE_PATH/datasets/detectron \
+#     -i /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/rgb \
+#     -m /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/csv \
+#     -s /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/detectron \
 #     -p run \
 #     -f 3 \
 #     -v None \
 #     --seed 42 \
 
-$BASE_PATH=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project
 # python3 utils/data_formatter.py \
-#     -i $BASE_PATH/datasets/master/NuCLSEvalSet/rgb \
-#     -m $BASE_PATH/datasets/master/NuCLSEvalSet/csv \
-#     -s $BASE_PATH/datasets/detectron_single \
+#     -i /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/rgb \
+#     -m /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/csv \
+#     -s /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/detectron_single \
 #     -p run \
 #     -f 3 \
 #     -v single \
 #     --seed 42 \
 
-$BASE_PATH=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project
 python3 utils/data_formatter.py \
-    -i $BASE_PATH/datasets/master/NuCLSEvalSet/rgb \
-    -m $BASE_PATH/datasets/master/NuCLSEvalSet/csv \
-    -s $BASE_PATH/datasets/detectron_three_class \
-    -mp $BASE_PATH/hypothesis_1/output/models/Xception_three_class/Xception_three_class.h5 \
+    -i /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/rgb \
+    -m /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/csv \
+    -s /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/datasets/detectron_three_class \
+    -mp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/hypothesis_1/output/models/Xception_three_class/Xception_three_class.h5 \
     -p run \
     -f 3 \
     -v three_class \
