252ab959431da450c8037d7e890f353f01090bcd
Aakash Rao <mrao.aakash@gmail.com>
2023-12-11 12:43:15 +0530

    idk tbh

--- yolo/plot.sh
+++ yolo/plot.sh
@@ -12,9 +12,9 @@
 #     -o $OUTDIR
 
 
-OLDDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/detectron/plots/faster_rcnn/csvs
-NEWDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/faster_rcnn/csvs
-OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/faster_rcnn/comparative
+OLDDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/detectron/plots/yolo/csvs
+NEWDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/yolo/csvs
+OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/yolo/comparative
 python3 utils/comparative.py \
     -d $OLDDIR \
     -o $OUTDIR \
