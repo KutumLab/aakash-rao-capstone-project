5dcac79ddd0c337383415d828be2e2525081464a
Aakash Rao <mrao.aakash@gmail.com>
2023-12-11 12:33:50 +0530

    idk tbh

--- yolo/plot.sh
+++ yolo/plot.sh
@@ -1,8 +1,8 @@
-# INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/yolo
-# OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/yolo/csvs
-# python3 utils/generate_csvs.py \
-#     -d $INDIR \
-#     -o $OUTDIR
+INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/outputs/yolo
+OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/yolo/csvs
+python3 utils/generate_csvs.py \
+    -d $INDIR \
+    -o $OUTDIR
 
 
 INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/yolo/csvs
@@ -13,8 +13,8 @@ python3 utils/generate_csvs.py \
 
 
 
-# INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/yolo/csvs
-# OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/yolo/plots
-# python3 utils/plot_yolo.py \
-#     -d $INDIR \
-#     -o $OUTDIR
+INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/yolo/csvs
+OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/yolo/plots
+python3 utils/plot_yolo.py \
+    -d $INDIR \
+    -o $OUTDIR
