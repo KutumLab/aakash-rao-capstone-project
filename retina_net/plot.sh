4f7a17f5d9e28766530aa4639282896c7e0cae63
Aakash Rao <mrao.aakash@gmail.com>
2023-12-12 10:18:39 +0530

    added stuff

--- retina_net/plot.sh
+++ retina_net/plot.sh
@@ -17,10 +17,10 @@ python3 utils/plot_retina_net.py \
     -d $INDIR \
     -o $OUTDIR
 
-OLDDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/detectron/retina_net/csvs
-NEWDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/csvs
-OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/comparative
-python3 utils/comparative.py \
-    -d $OLDDIR \
-    -o $OUTDIR \
-    -n $NEWDIR 
+# OLDDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/detectron/retina_net/csvs
+# NEWDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/csvs
+# OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/retina_net/comparative
+# python3 utils/comparative.py \
+#     -d $OLDDIR \
+#     -o $OUTDIR \
+#     -n $NEWDIR 
