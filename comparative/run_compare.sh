5851e795fec2356a308025c4b6ef2a5c4bdd478d
Aakash Rao <mrao.aakash@gmail.com>
2023-12-12 13:13:34 +0530

    mfdf

--- comparative/run_compare.sh
+++ comparative/run_compare.sh
@@ -6,12 +6,12 @@
 #     -m faster_rcnn \
 
 
-# python3 compare.py \
-#     -fcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_1 \
-#     -tcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_3 \
-#     -sp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_2 \
-#     -o /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/comparisons \
-#     -m retina_net \
+python3 compare.py \
+    -fcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_1 \
+    -tcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_3 \
+    -sp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_2 \
+    -o /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/comparisons \
+    -m retina_net \
 
 
 python3 compare.py \
