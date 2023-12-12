0c95cdc6a6e36837cf9ada37084f8bf10219269c
Aakash Rao <mrao.aakash@gmail.com>
2023-12-12 12:45:37 +0530

    mfdf

--- comparative/run_compare.sh
+++ comparative/run_compare.sh
@@ -6,17 +6,17 @@
 #     -m faster_rcnn \
 
 
-# python3 compare.py \
-#     -fcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_1 \
-#     -tcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_3 \
-#     -sp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_2 \
-#     -o /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/comparisons \
-#     -m retina_net \
-
-
 python3 compare.py \
-    -fcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/experiment_1 \
-    -tcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/experiment_3 \
-    -sp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/experiment_2 \
-    -o /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/comparisons \
-    -m yolo \
+    -fcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_1 \
+    -tcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_3 \
+    -sp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_2 \
+    -o /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/comparisons \
+    -m retina_net \
+
+
+# python3 compare.py \
+#     -fcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/experiment_1 \
+#     -tcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/experiment_3 \
+#     -sp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/experiment_2 \
+#     -o /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/comparisons \
+#     -m yolo \
