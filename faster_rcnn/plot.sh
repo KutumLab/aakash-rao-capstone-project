64cf7761f24274bf60237967278d011bc915f8cf
Aakash Rao <mrao.aakash@gmail.com>
2023-12-12 12:00:17 +0530

    added stuff

--- faster_rcnn/plot.sh
+++ faster_rcnn/plot.sh
@@ -11,11 +11,11 @@ python3 utils/generate_csv.py \
 
 
 
-INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/faster_rcnn/csvs
-OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/faster_rcnn/plots
-python3 utils/plot_faster_rcnn.py \
-    -d $INDIR \
-    -o $OUTDIR
+# INDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/faster_rcnn/csvs
+# OUTDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/faster_rcnn/plots
+# python3 utils/plot_faster_rcnn.py \
+#     -d $INDIR \
+#     -o $OUTDIR
 
 # OLDDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/detectron/plots/faster_rcnn/csvs
 # NEWDIR=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-capstone-project/plots/faster_rcnn/csvs
--- faster_rcnn/utils/plot_faster_rcnn.py
+++ faster_rcnn/utils/plot_faster_rcnn.py
@@ -9,7 +9,7 @@ axis = ['mAP', 'mAP', 'mAP', 'Accuracy', 'Iteration', 'Loss', 'Loss', 'Loss', 'L
 title_dict = dict(zip(cols, titles))
 y_axis_dict = dict(zip(cols, axis))
 x_axis_dict = "No. of Iterations"
-model_list = ['faster_rcnn_R_50_DC5_3x']#['faster_rcnn_R_101_DC5_3x','faster_rcnn_R_50_C4_1x','faster_rcnn_R_50_C4_3x','faster_rcnn_R_50_DC5_1x','faster_rcnn_R_50_DC5_3x']
+model_list = ['faster_rcnn_R_50_DC5_3x_three_class']#['faster_rcnn_R_101_DC5_3x','faster_rcnn_R_50_C4_1x','faster_rcnn_R_50_C4_3x','faster_rcnn_R_50_DC5_1x','faster_rcnn_R_50_DC5_3x']
 names = ['ResNet50 with DC5 at 3x']  #['ResNet101 with DC5 at 3x', 'ResNet50 with C4 at 1x', 'ResNet50 with C4 at 3x', 'ResNet50 with DC5 at 1x', 'ResNet50 with DC5 at 3x']
 model_dict = dict(zip(model_list, names))
 
