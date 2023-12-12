0b150841a92be2f1dbd848632952ba5554674d93
Aakash Rao <mrao.aakash@gmail.com>
2023-12-12 12:10:49 +0530

    mfdf

--- faster_rcnn/train_faster_rcnn_four_class.sh
+++ faster_rcnn/train_faster_rcnn_four_class.sh
@@ -5,6 +5,40 @@ SAVE_PATH=/media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/aakash-rao-c
 MAX_ITERS=15000
 
 
+# VARIABLE INFO faster_rcnn_R_50_DC5_3x [DONE]
+CONFIG=COCO-Detection/faster_rcnn_R_50_DC5_3x.yaml
+NAME=faster_rcnn_R_50_DC5_3x
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 1 \
+    --save_path $SAVE_PATH \
+    --version single
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 2 \
+    --save_path $SAVE_PATH \
+    --version single
+
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 3 \
+    --save_path $SAVE_PATH \
+    --version single
+
+
+
+
 # # VARIABLE INFO [DONE]
 # CONFIG=COCO-Detection/faster_rcnn_R_50_DC5_1x.yaml
 # NAME=faster_rcnn_R_50_DC5_1x
@@ -124,38 +158,6 @@ MAX_ITERS=15000
 #     --fold 3 \
 #     --save_path $SAVE_PATH \
 
-
-
-# # VARIABLE INFO faster_rcnn_R_50_DC5_3x [DONE]
-# CONFIG=COCO-Detection/faster_rcnn_R_50_DC5_3x.yaml
-# NAME=faster_rcnn_R_50_DC5_3x
-
-# python3 train_faster_rcnn.py \
-#     --data_path $DATA_PATH \
-#     --config_info $CONFIG \
-#     --max_iters $MAX_ITERS \
-#     --name $NAME \
-#     --fold 1 \
-#     --save_path $SAVE_PATH \
-#     --version single
-
-# python3 train_faster_rcnn.py \
-#     --data_path $DATA_PATH \
-#     --config_info $CONFIG \
-#     --max_iters $MAX_ITERS \
-#     --name $NAME \
-#     --fold 2 \
-#     --save_path $SAVE_PATH \
-#     --version single
-
-# python3 train_faster_rcnn.py \
-#     --data_path $DATA_PATH \
-#     --config_info $CONFIG \
-#     --max_iters $MAX_ITERS \
-#     --name $NAME \
-#     --fold 3 \
-#     --save_path $SAVE_PATH \
-#     --version single
 
 
 
--- faster_rcnn/train_faster_rcnn_three_class.sh
+++ faster_rcnn/train_faster_rcnn_three_class.sh
@@ -11,14 +11,14 @@ VERSION=three_class
 CONFIG=COCO-Detection/faster_rcnn_R_50_DC5_3x.yaml
 NAME=faster_rcnn_R_50_DC5_3x_three_class
 
-# python3 train_faster_rcnn.py \
-#     --data_path $DATA_PATH \
-#     --config_info $CONFIG \
-#     --max_iters $MAX_ITERS \
-#     --name $NAME \
-#     --fold 1 \
-#     --save_path $SAVE_PATH \
-#     --version $VERSION
+python3 train_faster_rcnn.py \
+    --data_path $DATA_PATH \
+    --config_info $CONFIG \
+    --max_iters $MAX_ITERS \
+    --name $NAME \
+    --fold 1 \
+    --save_path $SAVE_PATH \
+    --version $VERSION
 
 python3 train_faster_rcnn.py \
     --data_path $DATA_PATH \
