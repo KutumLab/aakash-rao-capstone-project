55dbf923f72b7b229d5cf624847da34d189e24d3
Aakash Rao <mrao.aakash@gmail.com>
2023-12-12 13:38:44 +0530

    mfdf

--- comparative/compare.py
+++ comparative/compare.py
@@ -68,13 +68,67 @@ def plot_metric(four_class_path, three_class_path, single_path, output_dir, mode
         
     pass
 
+def plot_metric_raw_weights(three_class_raw, three_class_coco, output_dir, model):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    archi = { 'faster_rcnn': 'faster_rcnn_R_50_DC5_3x', 'retina_net': 'retinanet_R_101_FPN_3x', 'yolo': 'yolov5m'}
+    if model =='yolo':
+        cols = [ 'mAP_5095','mAP_50', 'epoch']
+        three_class_raw_mean = pd.read_csv(os.path.join(three_class_raw, model, 'csvs',archi[model], f'mean_{archi[model]}.csv'), header=0).iloc[:100]
+        three_class_coco_mean = pd.read_csv(os.path.join(three_class_coco, model, 'csvs',archi[model], f'mean_{archi[model]}.csv'), header=0).iloc[:100]
+        three_class_raw_std = pd.read_csv(os.path.join(three_class_raw, model, 'csvs',archi[model], f'std_{archi[model]}.csv'), header=0).iloc[:100]
+        three_class_coco_std = pd.read_csv(os.path.join(three_class_coco, model, 'csvs',archi[model], f'std_{archi[model]}.csv'), header=0).iloc[:100]
+    else:
+        cols = ['AP5095', 'AP50', 'iteration']
+        three_class_raw_mean = pd.read_csv(os.path.join(three_class_raw, model, 'csvs',archi[model], f'mean_{archi[model]}.csv'), header=0).iloc[:100]/100
+        three_class_coco_mean = pd.read_csv(os.path.join(three_class_coco, model, 'csvs',archi[model], f'mean_{archi[model]}.csv'), header=0).iloc[:100]/100
+        three_class_raw_std = pd.read_csv(os.path.join(three_class_raw, model, 'csvs',archi[model], f'std_{archi[model]}.csv'), header=0).iloc[:100]/100
+        three_class_coco_std = pd.read_csv(os.path.join(three_class_coco, model, 'csvs',archi[model], f'std_{archi[model]}.csv'), header=0).iloc[:100]/100
+    titles = ['mAP@50:95', 'mAP@50',  'Iteration']
+    axis = ['mAP', 'mAP',  'Iteration']
+    title_dict = dict(zip(cols, titles))
+    y_axis_dict = dict(zip(cols, axis))
+    x_axis_dict = "No. of Epochs"
+    model_list = ['faster_rcnn','retina_net', 'yolo']
+    names = ['ResNet50 with FPN at 3x', 'ResNet101 with FPN at 3x', 'YOLOv5 M']
+    model_dict = dict(zip(model_list, names))
+    
+    for column in cols:
+        if column == 'iteration' or 'unnamed' in column.lower():
+            
+            continue
+        plt.figure(figsize=(4,4))
+        plt.locator_params(axis='x', nbins=5)
+        plt.locator_params(axis='y', nbins=5)
+        plt.grid(alpha=0.5, linestyle='--', linewidth=0.75) 
+        plt.plot(three_class_raw_mean[column].dropna().index, three_class_raw_mean[column].dropna(), label='Random Weights', color='blue', linewidth=0.75)
+        plt.plot(three_class_coco_mean[column].dropna().index, three_class_coco_mean[column].dropna(), label='COCO Weights', color='orange', linewidth=0.75)
+        plt.legend(loc='upper left', fontsize=10)
+        plt.fill_between(three_class_raw_std[column].dropna().index, three_class_raw_mean[column].dropna() - three_class_raw_std[column].dropna(), three_class_raw_mean[column].dropna() + three_class_raw_std[column].dropna(), alpha=0.25, color='blue')
+        plt.fill_between(three_class_coco_std[column].dropna().index, three_class_coco_mean[column].dropna() - three_class_coco_std[column].dropna(), three_class_coco_mean[column].dropna() + three_class_coco_std[column].dropna(), alpha=0.25, color='orange')
+
+        plt.title(f'{title_dict[column]} for {model}', fontsize=12, fontweight='bold')
+        plt.xlabel(x_axis_dict, fontsize=12, fontweight='bold')
+        plt.ylabel(y_axis_dict[column], fontsize=12, fontweight='bold')
+        plt.xticks(fontsize=10)
+        plt.yticks(fontsize=10)
+        plt.ylim(0,1)
+        plt.tight_layout()
+        plt.savefig(os.path.join(output_dir, f'{column}.png'), dpi=300)
+        plt.close()
+
+        
+    pass
+
 if __name__ == '__main__':
     argparse = argparse.ArgumentParser()
     argparse.add_argument('-fcp','--four_class_path', type=str, default='data')
     argparse.add_argument('-tcp','--three_class_path', type=str, default='data')
     argparse.add_argument('-sp','--single_path', type=str, default='data')
     argparse.add_argument('-o','--output_dir', type=str, default='data')
+    argparse.add_argument('-rw','--raw_weights', type=str, default='faster_rcnn_R_50_DC5_3x')
     argparse.add_argument('-m','--model', type=str, default='faster_rcnn_R_50_DC5_3x')
 
     args = argparse.parse_args()
-    plot_metric(args.four_class_path, args.three_class_path, args.single_path, args.output_dir, args.model)
+    # plot_metric(args.four_class_path, args.three_class_path, args.single_path, args.output_dir, args.model)
+    plot_metric_raw_weights(args.raw_weights, args.three_class_path, args.output_dir, args.model)
--- comparative/run_compare.sh
+++ comparative/run_compare.sh
@@ -6,17 +6,18 @@
 #     -m faster_rcnn \
 
 
-python3 compare.py \
-    -fcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_1 \
-    -tcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_3 \
-    -sp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_2 \
-    -o /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/comparisons \
-    -m retina_net \
+# python3 compare.py \
+#     -fcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_1 \
+#     -tcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_3 \
+#     -sp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/experiment_2 \
+#     -o /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/retina_net/comparisons \
+#     -m retina_net \
 
 
 python3 compare.py \
     -fcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/experiment_1 \
     -tcp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/experiment_3 \
     -sp /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/experiment_2 \
-    -o /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/comparisons \
+    -o /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/comparisons_raw_vs_coco \
     -m yolo \
+    -rw /media/chs.gpu/DATA/hdd/chs.data/research-cancerPathology/capstone_run_archive/yolo/experiment_4
