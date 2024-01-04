16bf607b03ce4d6f32ec496f8546d1e41c133a49
Aakash Rao <mrao.aakash@gmail.com>
2024-01-04 15:54:08 +0530

    added

--- faster_rcnn/run_data_formatter.sh
+++ faster_rcnn/run_data_formatter.sh
@@ -44,21 +44,21 @@ python3 utils/data_formatter.py \
     -v None \
     --seed 42 \
 
-# python3 utils/data_formatter.py \
-#     -i $BASE_PATH/datasets/master/NuCLSEvalSet/rgb \
-#     -m $BASE_PATH/datasets/master/NuCLSEvalSet/csv \
-#     -s $BASE_PATH/datasets/detectron_single \
-#     -p run \
-#     -f 3 \
-#     -v single \
-#     --seed 42 \
+python3 utils/data_formatter.py \
+    -i $BASE_PATH/datasets/master/NuCLSEvalSet/rgb \
+    -m $BASE_PATH/datasets/master/NuCLSEvalSet/csv \
+    -s $BASE_PATH/datasets/detectron_single \
+    -p run \
+    -f 3 \
+    -v single \
+    --seed 42 \
 
-# python3 utils/data_formatter.py \
-#     -i $BASE_PATH/datasets/master/NuCLSEvalSet/rgb \
-#     -m $BASE_PATH/datasets/master/NuCLSEvalSet/csv \
-#     -s $BASE_PATH/datasets/detectron_three_class \
-#     -mp $BASE_PATH/hypothesis_1/output/models/Xception_three_class/Xception_three_class.h5 \
-#     -p run \
-#     -f 3 \
-#     -v three_class \
-#     --seed 42 \
+python3 utils/data_formatter.py \
+    -i $BASE_PATH/datasets/master/NuCLSEvalSet/rgb \
+    -m $BASE_PATH/datasets/master/NuCLSEvalSet/csv \
+    -s $BASE_PATH/datasets/detectron_three_class \
+    -mp $BASE_PATH/hypothesis_1/output/models/Xception_three_class/Xception_three_class.h5 \
+    -p run \
+    -f 3 \
+    -v three_class \
+    --seed 42 \
