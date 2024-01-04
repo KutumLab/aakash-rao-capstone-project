4cde63f12d53de4a07320ee08ca18e075c1b0181
Aakash Rao <mrao.aakash@gmail.com>
2024-01-04 15:28:46 +0530

    added

--- faster_rcnn/run_data_formatter.sh
+++ faster_rcnn/run_data_formatter.sh
@@ -8,31 +8,30 @@
 module load compiler/anaconda3
 source /home/rintu.kutum/.conda/envs/detectron
 
-$BASE_PATH=/storage/bic/Aakash/aakash-rao-capstone-project
 # multiline run data formatter
 python3 utils/data_formatter.py \
-    -i $BASE_PATH/datasets/master/NuCLSEvalSet/rgb \
-    -m $BASE_PATH/datasets/master/NuCLSEvalSet/csv \
-    -s $BASE_PATH/datasets/detectron \
+    -i /storage/bic/Aakash/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/rgb \
+    -m /storage/bic/Aakash/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/csv \
+    -s /storage/bic/Aakash/aakash-rao-capstone-project/datasets/detectron \
     -p run \
     -f 3 \
     -v None \
     --seed 42 \
 
 python3 utils/data_formatter.py \
-    -i $BASE_PATH/datasets/master/NuCLSEvalSet/rgb \
-    -m $BASE_PATH/datasets/master/NuCLSEvalSet/csv \
-    -s $BASE_PATH/datasets/detectron_single \
+    -i /storage/bic/Aakash/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/rgb \
+    -m /storage/bic/Aakash/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/csv \
+    -s /storage/bic/Aakash/aakash-rao-capstone-project/datasets/detectron_single \
     -p run \
     -f 3 \
     -v single \
     --seed 42 \
 
 python3 utils/data_formatter.py \
-    -i $BASE_PATH/datasets/master/NuCLSEvalSet/rgb \
-    -m $BASE_PATH/datasets/master/NuCLSEvalSet/csv \
-    -s $BASE_PATH/datasets/detectron_three_class \
-    -mp $BASE_PATH/hypothesis_1/output/models/Xception_three_class/Xception_three_class.h5 \
+    -i /storage/bic/Aakash/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/rgb \
+    -m /storage/bic/Aakash/aakash-rao-capstone-project/datasets/master/NuCLSEvalSet/csv \
+    -s /storage/bic/Aakash/aakash-rao-capstone-project/datasets/detectron_three_class \
+    -mp /storage/bic/Aakash/aakash-rao-capstone-project/hypothesis_1/output/models/Xception_three_class/Xception_three_class.h5 \
     -p run \
     -f 3 \
     -v three_class \
