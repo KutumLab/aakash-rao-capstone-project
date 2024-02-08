ad8dacc5052e4a17b01100c35e5352bd90cf1a2b
Aakash Rao <mrao.aakash@gmail.com>
2024-02-08 11:26:36 +0530

    added correct x101 config path

--- faster_rcnn/plot.py
+++ faster_rcnn/plot.py
@@ -125,9 +125,9 @@ def plot(outpath, model_name):
         x = col_mean.index
         col_sem = sem[col].values[x]
         print (col_mean)
-        
+
         fig, ax = plt.subplots()
-        ax.plot(x, col_mean, label='Mean', marker='o', markersize=0.001)
+        ax.plot(x, col_mean, label='Mean', marker='o', markersize=0.001, linewidth=1)
         ax.fill_between(x, col_mean - col_sem, col_mean + col_sem, alpha=0.2, label='SEM')
 
         ax.set_ylabel('Iteration')
