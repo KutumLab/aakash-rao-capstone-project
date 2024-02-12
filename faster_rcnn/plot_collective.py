68c04450a5907b1cc73acccdb02167c416d8e754
Aakash Rao <mrao.aakash@gmail.com>
2024-02-12 10:38:16 +0530

    Added Xception

--- faster_rcnn/plot_collective.py
+++ faster_rcnn/plot_collective.py
@@ -320,7 +320,25 @@ def plot_new(outpath, model_names):
         EFGH
         """
     )
-    axd.savefig(os.path.join(savepath, 'collective.png'), bbox_inches='tight', dpi=300)
+    def identify_axes(ax_dict, fontsize=48):
+        """
+        Helper to identify the Axes in the examples below.
+
+        Draws the label in a large font in the center of the Axes.
+
+        Parameters
+        ----------
+        ax_dict : dict[str, Axes]
+            Mapping between the title / label and the Axes.
+        fontsize : int, optional
+            How big the label should be.
+        """
+        kw = dict(ha="center", va="center", fontsize=fontsize, color="darkgrey")
+        for k, ax in ax_dict.items():
+            ax.text(0.5, 0.5, k, transform=ax.transAxes, **kw)
+
+    fig = identify_axes(axd, fontsize=8)
+    fig.savefig(os.path.join(savepath, 'collective.png'), bbox_inches='tight', dpi=300)
     
 
         
