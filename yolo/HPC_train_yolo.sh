32103daf40d3b1a3f3343ee4181c35494bee7387
Aakash Rao <mrao.aakash@gmail.com>
2024-02-15 13:47:39 +0530

    added three class one

--- yolo/HPC_train_yolo.sh
+++ yolo/HPC_train_yolo.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-#PBS -N Four_Class_Faster_RCNN
+#PBS -N Four_Class_YOLO
 #PBS -o out_four_class.log
 #PBS -e err_four_class.log
 #PBS -l ncpus=50
-#PBS -q gpu
+#PBS -q cpu
 
 eval "$(conda shell.bash hook)"
 conda activate yolov5
@@ -15,6 +15,12 @@ test_dict["yolov5s"]="yolov5s.pt"
 test_dict["yolov5m"]="yolov5m.pt"
 test_dict["yolov5l"]="yolov5l.pt"
 test_dict["yolov5x"]="yolov5x.pt"
+test_dict["yolov5n6"]="yolov5n6.pt"
+test_dict["yolov5s6"]="yolov5s6.pt"
+test_dict["yolov5m6"]="yolov5m6.pt"
+test_dict["yolov5l6"]="yolov5l6.pt"
+test_dict["yolov5x6"]="yolov5x6.pt"
+
 
 IMG_SIZE=640
 EPOCHS=200
@@ -22,12 +28,12 @@ DEVICE=0
 BATCH_SIZE=16
 SAVE_PERIOD=10
 SAVE_PATH=/storage/bic/Aakash/aakash-rao-capstone-project/outputs/yolov5
-YAML_PATH=/storage/bic/Aakash/aakash-rao-capstone-project/yolo/configs
+YAML_PATH=/storage/bic/Aakash/aakash-rao-capstone-project/yolo/configs  
 
-models=("yolov5s") # "yolov5m" "yolov5l" "yolov5x")
+models=("yolov5s") #"yolov5m" "yolov5l" "yolov5x" "yolov5n6" "yolov5s6" "yolov5m6" "yolov5l6" "yolov5x6")
 for model_name in "${models[@]}"
 do
-    for FOLD in 0 #1 2 3 4
+    for FOLD in 1 2 3
     do
         NAME="$model_name"_"$VERSION" #Specify a name for the model used for saving and logistics
         WEIGHT=$BASE_WEIGHT_PATH/${test_dict[$model_name]}
@@ -36,16 +42,15 @@ do
 
         echo $NAME
         echo $WEIGHT
-        python ./yolov5/train.py \
+        python /storage/bic/Aakash/aakash-rao-capstone-project/yolo/yolov5/train.py \
             --img $IMG_SIZE \
             --epochs $EPOCHS \
             --data $YAML_FOLD \
-            --weights yolov5x.pt \
+            --weights /storage/bic/Aakash/aakash-rao-capstone-project/yolo/bases/$test_dict[$model_name] \
             --device $DEVICE \
             --batch-size $BATCH_SIZE \
             --project $PROJECT \
             --save-period $SAVE_PERIOD \
-            --name yolov5x-fold_3 \
-            --mode setup
+            --name $model_name-fold_3 
     done
 done
